51ac3b884b1f719cf8d490976306a3911faeacf9
Steven Than <steventhan11@gmail.com>
2016-10-09 15:46:40 -0700

    RPiLock class: io_client starts wating forever when listen_to_signal is invoked

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ SERVER, PORT = '52.43.75.183', 8000
 def main():
     rpi_lock = RPiLock(pigpio.pi(), SocketIO(SERVER, PORT))
     rpi_lock.listen_to_signal()
-    rpi_lock.io_client.wait()
 
 if __name__ == '__main__':
     main()
