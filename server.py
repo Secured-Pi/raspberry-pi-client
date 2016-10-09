ffa55ca84deb41153c0e1364a9e0018aa380d10e
Steven Than <steventhan11@gmail.com>
2016-10-08 18:09:00 -0700

    stop pigpio when not in use

--- server.py
+++ server.py
@@ -2,18 +2,21 @@ from flask import Flask
 import pigpio
 
 app = Flask(__name__)
-pi = pigpio.pi()
 
 
 @app.route('/left')
 def turn_left():
+    pi = pigpio.pi()
     pi.set_servo_pulsewidth(18, 500)
-    return 'Turned left'
+    pi.stop()
+    return pi.get_servo_pulsewidth()
 
 
 @app.route('/right')
 def turn_right():
+    pi = pigpio.pi()
     pi.set_servo_pulsewidth(18, 2500)
+    pi.stop()
     return 'Turned right'
 
 if __name__ == '__main__':
