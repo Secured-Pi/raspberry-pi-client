3c52ab8889a9d056d204c8161254853e680e5dc6
Steven Than <steventhan11@gmail.com>
2016-10-09 00:44:20 -0700

    incoporated socketio client

--- server.py
+++ server.py
@@ -1,23 +1,30 @@
+# coding: utf-8
 from flask import Flask
+from socketIO_client import SocketIO
 import pigpio
 
+io_client = SocketIO('52.43.75.183', 8000)
 app = Flask(__name__)
 
 
-@app.route('/left')
-def turn_left():
+@app.route('/unlock')
+def unlock():
     pi = pigpio.pi()
-    pi.set_servo_pulsewidth(18, 500)
+    pi.set_servo_pulsewidth(18, 600)
     pi.stop()
-    return 'Turned left'
+    return 'Door unlocked'
 
 
-@app.route('/right')
-def turn_right():
+@app.route('/lock')
+def lock():
     pi = pigpio.pi()
-    pi.set_servo_pulsewidth(18, 2500)
+    pi.set_servo_pulsewidth(18, 2400)
     pi.stop()
-    return 'Turned right'
+    return 'Door locked'
+
+io_client.on('unlock', unlock)
+io_client.on('lock', lock)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+    io_client.wait()
