f7362bd07d318667d194f73e8ac71ed7bf7fc6d0
bl41r <dbsmith.dbs83@gmail.com>
2016-10-11 05:36:40 -0700

    code cleanup

--- src/webcam_face_detector.py
+++ src/webcam_face_detector.py
@@ -8,7 +8,6 @@ https://pythonprogramming.net/loading-video-python-opencv-tutorial/
 """
 
 import cv2
-import sys
 import logging as log
 import datetime as dt
 
@@ -16,58 +15,55 @@ CASCADE_MODEL = 'haarcascade_frontalface_default.xml'
 FACE_CASCADE = cv2.CascadeClassifier(CASCADE_MODEL)
 log.basicConfig(filename='entries.log', level=log.INFO)
 
+
 def send_img_to_server(img_filename):
-	print('image sent to server!')
-	pass
+    print('image sent to server!')
+    pass
 
 
 def begin_watch(debug=False):
-	"""Begin watching the camera for visitors.
-	
-	If a person's face is found, it is saved as a file and then send off
-	to ther server to be validated.  When debug is True, the camera 
-	output is displayed on the screen.
-	"""
-	
-	video_capture = cv2.VideoCapture(0)
-	video_capture.set(3, 640)
-	video_capture.set(4, 480)
-	num_faces_state = 0
+    """Begin watching the camera for visitors.
 
-	while True:
-		ret, frame = video_capture.read()
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		faces = FACE_CASCADE.detectMultiScale(
-			gray,
-			scaleFactor=1.1,
-			minNeighbors=5,
-			minSize=(30, 30)
-		)
-		
-		if debug:
-			for (x, y, w, h) in faces:
-				cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2) # black and white
-				# cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) #  for color
+    If a person's face is found, it is saved as a file and then send off
+    to ther server to be validated.  When debug is True, the camera
+    output is displayed on the screen.
+    """
 
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(3, 640)
+    video_capture.set(4, 480)
+    num_faces_state = 0
 
-		# for logging
-		# print(num_faces_state)
-		if num_faces_state != len(faces):
-			num_faces_state = len(faces)
-			if num_faces_state == 1:
-				cv2.imwrite('testing.png', gray)
-				print('picture taken!')
-				send_img_to_server('testing.png')
-				log.info(str(dt.datetime.now()) + '::face found.')
+    while True:
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = FACE_CASCADE.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
+        )
 
-		if debug:
-			cv2.imshow('Video', gray)   #black and white
-			# cv2.imshow('Video', frame) # for color
+        if debug:
+            for (x, y, w, h) in faces:
+                cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        # for logging
+        # print(num_faces_state)
+        if num_faces_state != len(faces):
+            num_faces_state = len(faces)
+            if num_faces_state == 1:
+                cv2.imwrite('testing.jpg', gray)
+                print('picture taken!')
+                send_img_to_server('testing.png')
+                log.info(str(dt.datetime.now()) + '::face found.')
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
+        if debug:
+            cv2.imshow('Video', gray)   # black and white
 
-	video_capture.release()
-	if debug:
-		cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
+    video_capture.release()
+    if debug:
+        cv2.destroyAllWindows()
