930ca9ddb095467b92d2bed7e67064af4e6b7cca
Steven Than <steventhan11@gmail.com>
2016-10-11 18:12:13 -0700

    added user class

--- src/lock.py
+++ src/lock.py
@@ -1,12 +1,15 @@
 # coding: utf-8
+import requests
 
 
 class RPiLock(object):
     """RPiLock class, a representation of the physical lock."""
-    def __init__(self, server, port, model='motorized'):
+    def __init__(self, user, server, port=80, model='motorized'):
         """RPiLock instance with socketio connection."""
         import pigpio
+        self.user = user
         self.serial = self.get_serial()
+        self.lock_id = self.get_lock_id()
         self.model = model
         self.server, self.port = server, port
         self.pi = pigpio.pi()
@@ -30,19 +33,27 @@ class RPiLock(object):
             raise IOError('Serial not found, make sure this is a RPi client')
         return serial
 
+    def get_lock_id(self):
+        req_url = 'http://{}:{}/api/locks/'.format(self.server, self.port)
+        all_locks = requests.post(
+            req_url,
+            auth=requests.auth.HTTPBasicAuth(
+                self.user.username,
+                self.user.password
+            )
+        ).json()
+        for lock in all_locks:
+            if lock['serial'] == self.serial:
+                return lock['pk']
+
     def update_serverside_status(self, data):
         """Update lock status on central server."""
-        import requests
         req_url = 'http://{}:{}/api/locks/{}'.format(
-            self.server, self.port, data['lock_id']
+            self.server, self.port, self.lock_id
         )
         return requests.post(req_url, json=data)
 
-    def handle_io_event(self, data):
-        """Handling socketio event coming from server."""
-        pass
-
-    def control_motor(self, action, pin_num=18):
+    def control_motorized(self, action, pin_num=18):
         """
         Output approriate motor control signal and pulswidth to the GPIO pins.
         """
@@ -52,17 +63,25 @@ class RPiLock(object):
         self.pi.set_servo_pulsewidth(pin_num, pulsewidth)
         return self.pi.get_servo_pulsewidth(pin_num)
 
-    def control_electromagnetic(self):
+    def control_electromagnetic(self, action):
         pass
 
-    def io_connect(self):
-        """Connect to socketio server."""
+    def handle_io_event(self, data):
+        """Handling socketio event coming from server."""
+        import sys
+        getattr(
+            sys.modules[__name__],
+            'control_{}'.format(self.model)
+        )(data['action'])
+        data['']
+        self.update_serverside_status(data)
+
+    def listen_for_io_signal(self):
+        """Establish a never-ending connection and listen to signal."""
         from socketIO_client import SocketIO
         self.io_client = SocketIO(self.server, self.port)
         self.io_client.emit('listening', {'serial': self.serial})
-
-    def listen_to_signal(self):
-        """Establish a never-ending connection and listen to signal."""
-        self.io_client.on('unlock', lambda x: self.control_motor('unlock'))
-        self.io_client.on('lock', lambda x: self.control_motor('lock'))
+        self.io_client.on('unlock', self.handle_io_event)
+        self.io_client.on('lock', self.handle_io_event)
+        print('Now listening to central server')
         self.io_client.wait()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from lock import RPiLock
+from user import User
 import logging
+import getpass
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 
 SERVER, PORT = '52.43.75.183', 8000
 
 
+def verify_user(server, port=None):
+    while True:
+        username = input('Secured Pi username: ')
+        password = getpass.getpass()
+        user = User(username, password, server, port)
+        res = user.login()
+        if res.status_code == 200:
+            print('Secured Pi accepts you!')
+            return user
+
+
 def main():
-    rpi_lock = RPiLock(SERVER, PORT)
-    rpi_lock.listen_to_signal()
+    user = verify_user(SERVER, PORT)
+    rpi_lock = RPiLock(user, SERVER, PORT)
+    rpi_lock.listen_for_io_signal()
 
 if __name__ == '__main__':
     main()
